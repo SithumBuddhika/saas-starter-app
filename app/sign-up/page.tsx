56beab1d9d1dd02da5b12e429c070656da64cb0d
Sithum Buddhika Jayalal <Officialsithumbuddhika@gmail.com>
2026-01-18 01:35:10 +0530

    Clerk Sign-up Half way done

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSignUp } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+function Signup() {
+  return <div>Signup</div>;
+}
+
+export default Signup;
